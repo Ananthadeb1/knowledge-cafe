a27e5d6d069cae393b0ec71203f57cc88a981ef3
Anantha Debnath <ananthadebnath103@gmail.com>
2023-05-04 00:17:27 +0600

    bookmark added

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,22 +1,45 @@
-import React from 'react';
-import "./Course.css"
+import React from "react";
+import "./Course.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Course = (props) => {
-    const { content_title, content_banner, author_name, author_profile, course_type } = props.course;
+    const {
+        content_title,
+        content_banner,
+        author_name,
+        author_profile,
+        course_type,
+        time,
+    } = props.course;
     const handleAddToCart = props.handleAddToCart;
-
+    function bookM(){
+        console.log("Book");
+    } 
 
     return (
-        <div className='course'>
-            <img src={content_banner} alt="" />
-            <div className='course-info'>
-                <h6 className='course-name'>{content_title}</h6>
-                <h3>{author_name}</h3>
+        <div className="course">
+            <img className="banner-img" src={content_banner} alt="" />
+            <div className="auth-sec">
+                <div className="author-info">
+                    <img className="auth-img" src={author_profile} alt="" />
+                    <h3>{author_name}</h3>
+                </div>
+                <div className="time">
+                    <p style={{cursor: "pointer"}} onClick={bookM}>{time} min read .
+                    <FontAwesomeIcon icon={faBookmark} />
+                    </p>
+                </div>
             </div>
-            <button onClick={() => handleAddToCart(props.course)} className='btn-cart'>
-                Add to Cart
-                {/* <FontAwesomeIcon icon={faShoppingCart} /> */}
-                </button>
+            <h1 className="course-name">{content_title}</h1>
+            <div className="course-info">
+                <p>{course_type}</p>
+            </div>
+
+            <p style={{cursor: "pointer"}}
+                onClick={() => handleAddToCart(props.course)}>
+                Mark as read
+            </p>
         </div>
     );
 };
